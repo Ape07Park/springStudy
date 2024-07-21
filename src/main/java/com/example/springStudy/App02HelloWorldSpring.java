9448546f6b6d85268105b9228d09ac51ac19ce84
Ape07Park <ape072399@gmail.com>
2024-07-21 18:46:50 +0900

    스프링 컨텍스트2

--- src/main/java/com/example/springStudy/App02HelloWorldSpring.java
+++ src/main/java/com/example/springStudy/App02HelloWorldSpring.java
@@ -22,16 +22,24 @@ public class App02HelloWorldSpring {
 //      @Configuration : 설정 클래스 만들고 이름 등을 정의
 
 
-
 //      3. 스프링에 의해 관리되는 빈 가져오기
         // 스프링(스프링 컨텍스트)이 관리하는 것은 무엇이든 bean임
-        // 컨텍스트에서 값을 가져오는 방법 중 1가지는 이름으로 가져오는 것 ex) 메서드 명
+        // 1) 컨텍스트에서 값을 가져오는 방법 중 1가지는 이름으로 가져오는 것 ex) 메서드 명
         System.out.println(context.getBean("name")); // 스프링이 관리 중이라 스프링 컨텍스트를 실행해 컨텍스트 안의 특정 빈을 가져오기 O
 
         System.out.println(context.getBean("age"));
 
         System.out.println(context.getBean("person"));
 
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("person2Method")); // 같은 기능을 가진 다른 이름의 메서드 호출
+
+//      2) 빈의 이름을 메서드가 아닌 내가 지정한 이름으로 불러오기
+        System.out.println(context.getBean("address2"));
+
+//      3) 빈의 클래스를 가지고 와 값 가져오기
+//        System.out.println(context.getBean(Address.class)); // 검색하고 있는 것이 빈의 유형(Address)이라 같은 유형이 두 개 이상이면 에러남
+
+        // 4) 각 빈을 특정 메서드의 파라미터로 받기
+        System.out.println(context.getBean("person3Parameters")); // 같은 기능을 가진 다른 이름의 메서드 호출
     }
 }
--- src/main/java/com/example/springStudy/HelloWorldConfiguration.java
+++ src/main/java/com/example/springStudy/HelloWorldConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-
 // record: 사용자 정의 클래스를 만들 때 getter 등이 필요. record는  getter, setter, 생성자, toString 자동 생성으로 자바 빈을 만드는 것 더 간결화해줌.
-record Person (String name, int age) { }
+record Person(String name, int age, Address address) {
+}
 
-record Address (String firstLine, String state,String city, String zip) { }
+record Address(String firstLine, String state, String city, String zip) {
+}
+
 /**
  * 설정 클래스
  * 설정 클래스 내부에서 메서드를 정의해 스프링 빈 생성
@@ -26,18 +28,45 @@ public class HelloWorldConfiguration {
     }
 
     @Bean // 스프링 컨테이너가 관리하는 빈을 생산함을 나타냄
-    public String age() {
-        return "15";
+    public int age() {
+        return 15;
     }
 
+    /**
+     * 이름으로 빈 호출하기
+     */
     @Bean
     public Person person() {
-        var person = new Person("Park", 25);
+        var person = new Person("Park", 25, new Address("korea", "경기", "의왕", "16666"));
         return person;
     }
 
+    // 같은 기능을 가진 다른 이름의 메서드 호출
     @Bean
+    public Person person2Method() {
+        return new Person(name(), age(), address());
+    }
+
+    /**
+     * 내가 지정한 이름으로 호출하기
+     * 빈의 이름을 메서드의 이름이 아닌 내가 지정한 이름으로 사용할 시 name=~~ 를 사용함
+     */
+    @Bean(name = "address2")
     public Address address() {
         return new Address("korea", "경기", "의왕", "16666");
+    }
+
+    /**
+     * 각 빈을 특정 메서드의 파라미터로 받기.
+     * 각 빈을 특정 메서드의 파라미터로 받아서 그 특정 메서드를 호출하면 파라미터의 빈들도 호출됨
+     */
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("US", "DC", "ciatle", "22222");
     }
 }
