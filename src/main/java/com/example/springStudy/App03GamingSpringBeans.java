447746a74bd2092e9baa4f161764be5543f08829
SUNGMIN PARK <ape072399@gmail.com>
2024-07-26 09:33:27 +0900

    @component

--- src/main/java/com/example/springStudy/App03GamingSpringBeans.java
+++ src/main/java/com/example/springStudy/App03GamingSpringBeans.java
@@ -5,11 +5,30 @@ import com.example.springStudy.game.GamingConsole;
 import com.example.springStudy.game.MarioGame;
 import com.example.springStudy.game.PackmamGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+
+
+    /**
+     * @Component이 붙어 있는 곳이 어딘지 몰라 GamingConsole game 주입 실패. 따라서 @Component이 붙어 있는 곳 위치 알려야 함.
+     * 그 역할을 하는 것이 @ComponentScan
+      */
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+
+    /*
+    main 함수
+     */
     public static void main(String[] args) {
 
-       try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(GamingConfig.class)) {
+       try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
 
            context.getBean(GamingConsole.class).up();
 
--- src/main/java/com/example/springStudy/game/PackmamGame.java
+++ src/main/java/com/example/springStudy/game/PackmamGame.java
@@ -1,5 +1,10 @@
 package com.example.springStudy.game;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component // 스프링 컨텍스트에서 관리 따라서 spring이 자동으로 인스턴스 생성해줌, 개발자가 직접 작성한 Class를 Bean으로 등록하기 위한 어노테이션
+@ComponentScan // @Component이 붙어 있는 곳 위치 알리는 역할
 public class PackmamGame implements GamingConsole{
     @Override
     public void up() {
