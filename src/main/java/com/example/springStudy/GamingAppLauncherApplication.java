a1cba49f93fb7019fa65d6e2de5effea23bfa6f5
SUNGMIN PARK <ape072399@gmail.com>
2024-07-30 09:30:03 +0900

    @component

--- src/main/java/com/example/springStudy/GamingAppLauncherApplication.java
+++ src/main/java/com/example/springStudy/GamingAppLauncherApplication.java
@@ -23,6 +23,12 @@ public class GamingAppLauncherApplication {
 //        return gameRunner;
 //    }
 
+    // @Primary: 우선권 주기 여러 개의 자격이 있는 후보가 있을 떄
+    // @Qualifier: 특정 빈을 지정해 autoWiring함
+    // ** @Primary, @Qualifier를 선택할 때 항상 특정 의존성을 사용하는 클래스의 관점에서 생각할 것
+    // **   @Qualifier가 @Primary보다 더 높은 우선순위 지님
+    // @Autowired: 어떤 거든 상관 없으면 @Autowired 사용
+
     /*
     main 함수
      */
